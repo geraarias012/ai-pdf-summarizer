cd8f029f8730b0a410fc5c803f39b150fc1d5d7b
Gerardo Arias <ariasariasarias6@gmail.com>
2026-03-12 10:32:43 -0600

    connected pdf processing pipeline with streamlit interface

--- app/app.py
+++ app/app.py
@@ -1,37 +1,75 @@
 import streamlit as st
+import sys
+import os
+import re
 
-# Configuración de la página
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from src.pdf_reader.pdf_reader import extract_text_from_pdf
+from src.text_processing.text_chunker import fixed_chunking
+from src.summarizer.summarizer import summarize_half, summarize_quarter
+
+
 st.set_page_config(
     page_title="AI PDF Summarizer",
     page_icon="📄",
     layout="centered"
 )
 
-# Título
+
 st.title("📄 AI PDF Summarizer")
 st.write("Sube un archivo PDF y la IA generará un resumen automáticamente.")
 
-# Subir archivo PDF
+def chunking(uploaded_file):
+    text = extract_text_from_pdf(uploaded_file)
+
+    chunks = fixed_chunking(text)
+
+    return chunks
+
+
+def half_summarizer(chunks):
+
+    st.info("Generando resumen con IA...")
+
+    summaries = summarize_half(chunks)
+
+    return summaries
+
+
+def quarter_summarizer(chunks):
+
+
+    st.info("Generando resumen con IA...")
+
+    summaries = summarize_quarter(chunks)
+
+    return summaries
+
+def final_summaries(summaries):
+
+    final_summary = "\n".join(summaries)
+
+    st.subheader("Resumen generado")
+
+    st.write(final_summary)
+
+
 uploaded_file = st.file_uploader(
     "Sube tu archivo PDF",
     type=["pdf"]
 )
 
-# Botón para generar resumen
 if uploaded_file is not None:
 
     st.success("PDF cargado correctamente")
 
-    if st.button("Generar resumen", type="secondary"):
+    if st.button("Generar resumen 50%"):
+        chunks = chunking(uploaded_file)
+        summaries = half_summarizer(chunks)
+        final_summaries(summaries)
 
-        st.info("Procesando documento...")
-
-        # Aquí se leerá el pdf, entra el modelo y regresa el resumen del pdf 
-
-        resumen = """
-        Este es un ejemplo de resumen generado por la IA.
-        *Aquí deberá aparecer el pdf resumido luego de que haga el modelo*
-        """
-
-        st.subheader("Resumen generado")
-        st.write(resumen)
+    if st.button("Generar resumen 25%"):
+        chunks = chunking(uploaded_file)
+        summaries = quarter_summarizer(chunks)
+        final_summaries(summaries)
--- src/summarizer/summarizer.py
+++ src/summarizer/summarizer.py
@@ -1,22 +1,68 @@
-import ollama
+import ollama 
+import re
 
-def summarize_text(chunks):
+def summarize_half(chunks):
 
     summaries = []
 
     for chunk in chunks:
 
-        prompt = f"""
-        Resume el siguiente texto en español en máximo 5 líneas:
+        prompt = f'''
+        Resume el siguiente texto en español en máximo 80 palabras.
 
+        Reglas:
+        - Solo escribe el resumen.
+        - No escribas introducciones.
+        - No escribas frases como "Aquí te presento" o "A continuación".
+        - No agregues comentarios.
+
+        Texto:
         {chunk}
-        """
+        '''
 
         response = ollama.generate(
             model="llama3",
             prompt=prompt
         )
 
-        summaries.append(response["response"])
+        summary = response["response"]
+
+        summary = re.sub(r"Aquí te presento.*?:", "", summary)
+        summary = re.sub(r"A continuación.*?:", "", summary)
+
+        summaries.append(summary.strip())
+
+    return summaries
+
+def summarize_quarter(chunks):
+
+    summaries = []
+
+    for chunk in chunks:
+
+        prompt = f'''
+        Resume el siguiente texto en español en máximo 40 palabras.
+
+        Reglas:
+        - Solo escribe el resumen.
+        - No escribas introducciones.
+        - No escribas frases como "Aquí te presento" o "A continuación".
+        - No agregues comentarios.
+
+        Texto:
+        {chunk}
+        '''
+
+        response = ollama.generate(
+            model="llama3",
+            prompt=prompt
+        )
+
+        summary = response["response"]
+
+        summary = re.sub(r"Aquí te presento.*?:", "", summary)
+        summary = re.sub(r"A continuación.*?:", "", summary)
+
+        summaries.append(summary.strip())
 
     return summaries
